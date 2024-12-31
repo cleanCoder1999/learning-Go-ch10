a8836f72ad0bef1760e8c10b06e2cc7022d62833
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-31 15:53:05 +0100

    adds godoc comments

--- add.go
+++ add.go
@@ -1,5 +1,11 @@
+// Package add contains a function for adding integers
 package add
 
-func Add(a,b int) int {
+// Add takes two parameters of type int, adds them, and returns the sum
+//
+// for further details [see]
+//
+// [see]: https://www/mathsisfun.com/numbers/addition.html
+func Add(a, b int) int {
 	return a + b
 }
